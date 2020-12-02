f052386779214ebf5b6ad0c9a84b9a37afb95a8a
MikelLiza <mikellizarralde4@gmail.com>
2020-12-02 17:36:59 +0000

    First commit

---
+++ src/COM2040_lab2-1.cpp
@@ -0,0 +1,19 @@
+/*
+ * COM2040_lab2-1.cpp
+ *
+ *  Created on: 12 Nov 2020
+ *      Author: mikel
+ */
+#include <iostream>
+using namespace std;
+int main(){
+	char initialOne;
+	char initialTwo;
+	cout << "Enter first initial";
+	cin >> initialOne;
+	cout << "Enter second initial";
+	cin >> initialTwo;
+	cout <<"Hello, " << initialOne << " " << initialTwo << endl;
+}
+
+
